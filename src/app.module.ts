f5b4e4edb679d024ea1f4dba92b084f637f6c0c8
kimmangyu <vlsual0917@gmail.com>
2024-02-04 23:40:08 +0900

    morgan 적용/ api작성 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { MorganModule, MorganInterceptor } from "nest-morgan";
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
 
 @Module({
-  imports: [ConfigModule.forRoot( {isGlobal : true})],
+  imports: [ConfigModule.forRoot( {isGlobal : true}), MorganModule, UsersModule, WorkspacesModule, ChannelsModule, DmsModule],
   controllers: [AppController],
-  providers: [AppService, ConfigService],
+  providers: [AppService, {provide: APP_INTERCEPTOR, useClass: MorganInterceptor("combined")}],
 })
 export class AppModule {}
---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,12 @@
+// import { Injectable, NestMiddleware } from '@nestjs/common';
+// import { Request, Response, NextFunction } from 'express';
+// import * as morgan from 'morgan';
+
+// @Injectable()
+// export class LoggerMiddleware implements NestMiddleware {
+//   private readonly logger = morgan('tiny');
+
+//   use(req: Request, res: Response, next: NextFunction) {
+//     this.logger(req, res, next);
+//   }
+// }
